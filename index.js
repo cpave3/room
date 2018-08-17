7a96e5119ee156f5c4b63dffd1883e493bbc416a
Cameron Pavey <cpave3@gmail.com>
2018-08-17 17:23:14 +1000

    :recycle: Changed hard coded port to env based value

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// Requires
 const app = require('express')();
 const http = require('http')(app);
 const io = require('socket.io')(http);
 const Log = require('sleek-log')
     , log = new Log();
+
+// Config
+const port = process.env.PORT || 3000;
 
 // Return basic page on deault route
 app.get('/', (req, res) => {
@@ -17,6 +21,6 @@ io.on('connection', socket => {
 });
 
 
-http.lsiten(3000, () => {
-    log.info('listening on 3000');
+http.lsiten(port, () => {
+    log.info(`listening on ${port}`);
 });
